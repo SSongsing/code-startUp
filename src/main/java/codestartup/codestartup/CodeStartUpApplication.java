ffb802cc36cbf11fadf9025694fd866a59681498
Eunsong Kim <87457056+SSongsing@users.noreply.github.com>
2024-03-24 21:55:28 +0900

    feat: 초기 세팅

---
+++ src/main/java/codestartup/codestartup/CodeStartUpApplication.java
@@ -0,0 +1,13 @@
+package codestartup.codestartup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeStartUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeStartUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codestartup/codestartup/CodeStartUpApplicationTests.java
@@ -0,0 +1,13 @@
+package codestartup.codestartup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeStartUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
