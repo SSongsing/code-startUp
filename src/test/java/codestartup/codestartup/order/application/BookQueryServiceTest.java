3da629ca0189849176ca22ac012d6b3fb5810b6e
Eunsong Kim <87457056+SSongsing@users.noreply.github.com>
2024-03-26 21:12:05 +0900

    test: 책 구매 동작 테스트 코드

--- src/test/java/codestartup/codestartup/order/application/BookQueryServiceTest.java
+++ src/test/java/codestartup/codestartup/order/application/BookQueryServiceTest.java
@@ -1,15 +1,22 @@
 package codestartup.codestartup.order.application;
 
+import codestartup.codestartup.order.domain.Book;
 import codestartup.codestartup.order.domain.repository.BookRepository;
+import codestartup.codestartup.order.domain.view.GetBookListView;
+import codestartup.codestartup.order.domain.view.GetBookView;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
-
 class BookQueryServiceTest {
 
     @InjectMocks
@@ -17,23 +24,24 @@ class BookQueryServiceTest {
     @Mock
     BookRepository bookRepository;
 
-//    @Test
-//    void getBookList() {
-//        List<Book> bookList = new ArrayList<>();
-//        Book book = Book.builder()
-//                .id(1)
-//                .name("차근차근 개발")
-//                .category("개발")
-//                .price(5000000)
-//                .build();
-//        bookList.add(book);
-//        List<GetBookView> getBookViewList = new ArrayList<>();
-//        getBookViewList.add(GetBookView.builder().book(book).build());
-//        GetBookListView getBookListView = new GetBookListView(getBookViewList);
-//
-//        given(bookRepository.findAll()).willReturn(bookList);
-//        GetBookListView result = orderQueryService.getBookList();
-//
-//        assertThat(getBookListView).usingRecursiveComparison().isEqualTo(result);
-//    }
+    @Test
+    void getBookList() {
+        List<Book> bookList = new ArrayList<>();
+        Book book = Book.builder()
+                .id(1)
+                .name("차근차근 개발")
+                .category("개발")
+                .price(5000000)
+                .build();
+        bookList.add(book);
+        List<GetBookView> getBookViewList = new ArrayList<>();
+        List<Integer> discountList = new ArrayList<>();
+        getBookViewList.add(GetBookView.builder().book(book).discountList(discountList).build());
+        GetBookListView getBookListView = new GetBookListView(getBookViewList);
+
+        given(bookRepository.findAll()).willReturn(bookList);
+        GetBookListView result = bookQueryService.getBookList();
+
+        assertThat(getBookListView).usingRecursiveComparison().isEqualTo(result);
+    }
 }
--- src/test/java/codestartup/codestartup/order/interfaces/controller/OrderControllerTest.java
+++
@@ -1,7 +0,0 @@
-package codestartup.codestartup.order.interfaces.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class OrderControllerTest {
-
-}
