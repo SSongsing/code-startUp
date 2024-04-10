050be299b3c4ff1b5770b7889a16a067e578dce7
Eunsong Kim <87457056+SSongsing@users.noreply.github.com>
2024-04-10 14:52:35 +0900

    refactor: 할인 대상 카테고리 지정할 수 있도록 수정

---
+++ src/main/java/codestartup/codestartup/order/domain/discount/CategoryDiscountPolicy.java
@@ -0,0 +1,29 @@
+package codestartup.codestartup.order.domain.discount;
+
+import codestartup.codestartup.order.domain.Book;
+import codestartup.codestartup.order.domain.CategoryType;
+import codestartup.codestartup.order.domain.Money;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+public class CategoryDiscountPolicy implements DiscountPolicy{
+
+    public CategoryDiscountPolicy(String DISCOUNT_CATEGORY, Money DISCOUNT_AMOUNT) {
+        this.DISCOUNT_CATEGORY = DISCOUNT_CATEGORY;
+        this.DISCOUNT_AMOUNT = DISCOUNT_AMOUNT;
+    }
+    private String DISCOUNT_CATEGORY = CategoryType.IT.getValue();
+    private Money DISCOUNT_AMOUNT = new Money(1000);
+    @Override
+    public boolean isDiscountable(Book book) {
+        return StringUtils.equals(book.getCategory(), this.DISCOUNT_CATEGORY);
+    }
+
+    @Override
+    public Money getDiscountAmount(Book book) {
+        return DISCOUNT_AMOUNT;
+    }
+}
